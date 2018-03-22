55822ebfeeefe035d3c077633a2dca7874795425
Patrik  Jánosdeák <patrik.janosdeak@bistroagency.cz>
2018-03-22 18:47:46 +0100

    Imaginator - if template not found redirect to templates view

---
+++ src/Resources/views/templates.blade.php
@@ -0,0 +1,27 @@
+@extends('imaginator::layout.imaginator')
+
+@section('title', 'Šablony Imaginátorů')
+
+
+@section('content')
+<div class="ui one column centered middle aligned grid">
+	<div class="row">
+		<div class="four wide column">
+			<div class="ui fluid secondary message">
+				<p>Šablony Imaginátorů</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="four wide column">
+			<div class="ui fluid vertical menu">
+				@foreach($imaginatorTemplates as $template)
+					<a href="{{ route('imaginator.create', $template->name) }}" class="item">
+						{{ $template->label }}
+					</a>
+				@endforeach
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
