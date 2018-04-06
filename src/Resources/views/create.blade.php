06e9f0b797643010f8bcb8c9c4a7ac5836e70950
Patrik  Jánosdeák <patrik.janosdeak@bistroagency.cz>
2018-04-06 17:00:27 +0200

    General - gramatically correct text

--- src/Resources/views/create.blade.php
+++ src/Resources/views/create.blade.php
@@ -125,7 +125,7 @@
 							:class="{'disabled': !isSomethingSelected}"
 							style="text-align:left; margin-bottom:15px;">
 						<i class="folder open icon"></i>
-						Změnit soubor pro @{{ numberOfSelected }} variace
+						Změnit soubor pro @{{ numberOfSelected }} @{{ numberOfSelected > 1 ? 'variace' : 'variaci' }}
 					</button>
 
 					<button class="fluid large teal ui button"
@@ -133,7 +133,7 @@
 							v-if="isSomethingResizing"
 							style="text-align:left;">
 						<i class="check icon icon"></i>
-						Ořezat všechny vybrané variace
+						Ořezat vybrané variace
 					</button>
 
 				</div>
